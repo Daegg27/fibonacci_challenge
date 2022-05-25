6607b402f1a7c4e88397a31b6533fdd9376d0d6c
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-25 14:02:36 -0700

    did pyton

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
-  pass
+  fibArr = [0, 1]
+  answer = 0
 
+  for i in range(2, (n + 1)):
+    fibArr.append(fibArr[i - 1] + fibArr[i -2]) 
+  
+  answer = fibArr[n]
+  print(answer)
+  return answer
+
+fibonacci(4)
 
